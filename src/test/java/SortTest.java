3fd0813d3d35be2237fd1269338ba0e3cf8852ad
Alexandr <336743@mail.ru>
2022-02-19 16:56:37 +0300

    ignore

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -2,10 +2,6 @@
 import ru.sfedu.array_nasilie.CustomArrayList;
 import ru.sfedu.array_nasilie.Sort;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 /**
